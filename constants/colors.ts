596ee719360b4f2741f26a80bbcc92c95e024635
Mantas John <mantasjohnx@gmail.com>
2026-08-11 15:52:48 +0800

    commit

---
+++ constants/colors.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: '#166ee0',
+  background: '#050507',
+  card: '#0db94f',
+  textPrimary: '#ffffff',
+  textSecondary: '#ffffff',
+  textOnPrimary: '#FFFFFF',
+  surface: '#ffffff',
+  border: '#e1eaf3',
+  shadow: '#0261f0',
+  additional: 'rgb(255, 0, 0)'
+} as const;
